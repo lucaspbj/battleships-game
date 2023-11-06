85f8de8bf2b4d2bda4a9947ca95b779554b3bf12
Lucas Justino <lucaspbj@hotmail.com>
2023-11-06 21:20:28 +0000

    add function to place computer's ships

--- run.py
+++ run.py
@@ -9,3 +9,12 @@ def print_grid(grid):
     print("   " + " ".join(string.ascii_uppercase[:len(grid)]))
     for i, row in enumerate(grid):
         print(f"{i + 1:2} " + " ".join(row))
+
+def place_ships(grid, num_ships):
+    for _ in range(num_ships):
+        while True:
+            ship_row = random.randint(0, len(grid) - 1)
+            ship_col = random.randint(0, len(grid) - 1)
+            if grid[ship_row][ship_col] == 'O':
+                grid[ship_row][ship_col] = 'S'
+                break
