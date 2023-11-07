da7d7d8217cf3a7606b056f3f8c28d626914ebca
Lucas Justino <lucaspbj@hotmail.com>
2023-11-07 21:54:43 +0000

    Add comments for debugging
    - Added comments in the code to explain the logic.
    - Updated variable names for clarity.
    - Removed unnecessary print statements.

--- run.py
+++ run.py
@@ -1,22 +1,39 @@
 import random
 import string
 
-# Create the Game Grid
 def create_grid(size):
-    # Initialize the grid with 'O' (ocean) in all cells
+    """
+    Create a grid with a specified size.
+
+    Args:
+    size (int): The size of the grid.
+
+    Returns:
+    list: A 2D list representing the grid.
+    """
     grid = [['O' for _ in range(size)] for _ in range(size)]
     return grid
 
-# Display the Grid
 def print_grid(grid):
-    # Display the grid with row numbers and column letters
+    """
+    Display the game grid with computer's ships hidden.
+
+    Args:
+    grid (list): The 2D list representing the game grid.
+    """
+    visible_grid = [['O' if cell == 'S' else cell for cell in row] for row in grid]
     print("   " + " ".join(string.ascii_uppercase[:len(grid)]))
-    for i, row in enumerate(grid):
+    for i, row in enumerate(visible_grid):
         print(f"{i+1:2} " + " ".join(row))
 
-# Place Computer's Battleships
 def place_ships(grid, num_ships):
-    # Randomly place computer's battleships on the grid
+    """
+    Place computer's battleships on the grid.
+
+    Args:
+    grid (list): The 2D list representing the game grid.
+    num_ships (int): The number of battleships to place.
+    """
     for _ in range(num_ships):
         while True:
             ship_row = random.randint(0, len(grid) - 1)
@@ -25,20 +42,34 @@ def place_ships(grid, num_ships):
                 grid[ship_row][ship_col] = 'S'
                 break
 
-# Check Valid Guess
 def is_valid_guess(guess, size):
-    # Check if the player's guess is within the grid boundaries
+    """
+    Check if a player's guess is within the grid boundaries.
+
+    Args:
+    guess (tuple): A tuple representing the (row, col) guess.
+    size (int): The size of the grid.
+
+    Returns:
+    bool: True if the guess is valid, False otherwise.
+    """
     row, col = guess
     return 0 <= row < size and 0 <= col < size
 
-# Play the Game
 def play_battleships(size, num_ships):
-    player_grid = create_grid(size)
-    computer_grid = create_grid(size)
+    """
+    Play the Battleships game against the computer.
+
+    Args:
+    size (int): The size of the grid.
+    num_ships (int): The number of battleships to place.
+    """
+    grid = create_grid(size)
+    computer_grid = create_grid(size)  # Separate grid to track computer's ships
 
     print("Welcome to Battleships!")
     print("Here is your grid:")
-    print_grid(player_grid)
+    print_grid(grid)
 
     place_ships(computer_grid, num_ships)
 
@@ -56,21 +87,20 @@ def play_battleships(size, num_ships):
             print("Your guess is off-grid. Try again.")
             continue
 
-        if player_grid[row][col] == 'X':
+        if grid[row][col] == 'X':
             print("You've already guessed this location. Try again.")
             continue
 
         if computer_grid[row][col] == 'S':
             print("You hit a battleship!")
-            player_grid[row][col] = 'H'  # 'H' represents a hit on a battleship
+            grid[row][col] = 'H'  # 'H' represents a hit on a battleship
             computer_grid[row][col] = 'X' 
         else:
             print("You missed.")
-            player_grid[row][col] = 'X' # 'X' represents a missed shot
-
+            grid[row][col] = 'X' # 'X' represents a missed shot
 
         print("Your grid:")
-        print_grid(player_grid)
+        print_grid(grid)
 
         if 'S' not in [cell for row in computer_grid for cell in row]:
             print("Congratulations! You sank all the battleships. You win!")
